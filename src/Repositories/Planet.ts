152eefc01598813d26093ba0dc0e348cc7c2ecef
Batuhan Ozgur Ozdemir <batuhan@pankod.com>
2019-06-21 17:05:53 +0300

    refactor Planet repo

--- src/Repositories/Planet.ts
+++ src/Repositories/Planet.ts
@@ -1,9 +1,20 @@
 import { PlanetSql } from '@Interfaces';
-import { Planet } from '@Repositories/Models';
+import { Planet, Weapon } from '@Repositories/Models';
 import { CalculateMeta } from '@Meta';
 
 export module PlanetRepository {
 	export const Defend = async (damage: number): Promise<number> => {
-		return CalculateMeta.Damage(damage);
+		const weaponModel = await Weapon.Model();
+		const planetModel = await Planet.Model()
+
+		const deathStar: any = await weaponModel.findOne({ where: { name: 'Death Star' } })
+		const alderaan: any = await planetModel.findOne({ where: { name: "Alderaan" } })
+
+		const { remainingAmmo, remainingShield } = await CalculateMeta.Damage(deathStar, alderaan);
+
+		deathStar.update({ ammo: remainingAmmo })
+		alderaan.update({ shield: remainingShield })
+
+		return remainingShield
 	};
 }
