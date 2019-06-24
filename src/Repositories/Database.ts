bb497f44af4f43e470c5775b42e633fbd64b5490
Batuhan Ozgur Ozdemir <batuhan@pankod.com>
2019-06-24 11:21:32 +0300

    Fix db path

--- src/Repositories/Database.ts
+++ src/Repositories/Database.ts
@@ -3,7 +3,7 @@ import sequelize from 'sequelize';
 export module Database {
 	const sequelizeInstance = new sequelize({
 		dialect: 'sqlite',
-		storage: '../../db.sqlite'
+		storage: './db.sqlite'
 	});
 
 	export const instance = async (): Promise<sequelize.Sequelize> => {
