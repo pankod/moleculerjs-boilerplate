4003c86a8c032fc40dbf05c8d2fa81a9f778b184
Batuhan Ozgur Ozdemir <batuhan@pankod.com>
2019-06-25 16:57:15 +0300

    Refactor weapon Repo to use TypeORM

--- src/Repositories/Weapon.ts
+++ src/Repositories/Weapon.ts
@@ -1,12 +1,13 @@
-import { Weapon, Planet } from '@Models';
+import { Planet } from '@Entities/Planet'
+import { Weapon } from '@Entities/Weapon'
+import { getManager } from 'typeorm'
 
 export namespace WeaponRepository {
 	export const Fire = async (): Promise<{ damage: number; shield: number }> => {
-		const weaponModel = await Weapon.Model();
-		const planetModel = await Planet.Model();
+		const entityManager = getManager()
 
-		const deathStar: any = await weaponModel.findOne({ where: { name: 'Death Star' } });
-		const alderaan: any = await planetModel.findOne({ where: { name: 'Alderaan' } });
+		const deathStar = await entityManager.findOne(Weapon, { name: 'Death Star' })
+		const alderaan = await entityManager.findOne(Planet, { name: 'Alderaan' })
 
 		return { damage: deathStar.damage, shield: alderaan.shield };
 	};
