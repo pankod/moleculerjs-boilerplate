cdb3b3ea91b9b32494525231d42e5cf33a4f1ac4
Tamer Durgun <tamer@pankod.com>
2019-06-25 10:48:31 +0300

    repository tests completed

--- test/unit/Repositories/Planet.spec.ts
+++ test/unit/Repositories/Planet.spec.ts
@@ -1,16 +1,27 @@
 import { PlanetRepository } from '../../../src/Repositories/Planet';
-import { CalculateMeta } from '@Meta';
+import { Planet } from '@Models/Planet';
+import { PlanetSql } from '@Interfaces';
 
 describe('Test PlanetRepository constructor', () => {
+
 	it('should create an empty options', () => {
 		expect(PlanetRepository).toBeDefined();
 	});
 
 	it('should work Defend method', async () => {
-		const result = await PlanetRepository.Defend(1000);
+		const planetModel = await Planet.Model()
+		const planet = await planetModel.findOne({ where: { name: 'Alderaan' } }) as PlanetSql
 
-		const deathStarAttack = await CalculateMeta.getDeathStarAttack();
+		const damage = 1000
 
-			expect(result).toEqual(deathStarAttack.shield);
+		const expected: number = planet.shield - damage
+
+		const params = {
+			damage
+		};
+
+		const { alderaan } = await PlanetRepository.Defend(params.damage);
+
+		expect(alderaan.shield).toEqual(expected);
 	});
 });
