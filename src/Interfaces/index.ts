486e9f6a5cd977c82725ef86ffed9dc772ceb7a8
necati <necati@pankod.com>
2019-06-25 11:55:17 +0300

    Update model templates

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,7 +1,7 @@
 export { WeaponSql } from '@Interfaces/Weapon/WeaponSql';
 export { FireInDto } from '@Interfaces/Fire/FireInDto';
 export { FireOutDto } from '@Interfaces/Fire/FireOutDto';
-export { PlanetSql } from '@Interfaces/planet/PlanetSql';
+export { PlanetSql } from '@Interfaces/Planet/PlanetSql';
 export { DamageInDto } from '@Interfaces/Fire/DamageInDto';
 export { WarMessageOutDto } from '@Interfaces/Fire/WarMessageOutDto';
 export { FireResult } from '@Interfaces/Fire/FireResult';
