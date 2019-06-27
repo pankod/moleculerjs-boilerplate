a58cea0553a9a3cf9480b08bf90bcb04249c31fe
necati <necati@pankod.com>
2019-06-27 17:02:30 +0300

    Add example doc

--- moleculer.config.ts
+++ moleculer.config.ts
@@ -33,7 +33,7 @@ const brokerConfig: BrokerOptions = {
 
 	// Define transporter.
 	// More info: https://moleculer.services/docs/0.13/networking.html
-	transporter: "nats://localhost:4222",
+	transporter: "nats://localhost:32773",
 
 	// Define a serializer.
 	// Available values: "JSON", "Avro", "ProtoBuf", "MsgPack", "Notepack", "Thrift".
