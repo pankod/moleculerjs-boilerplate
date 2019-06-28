d27aaab331e7d0aff631e143c2a949cd4a20a6f8
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-06-28 11:58:56 +0300

    Planet repository spec

--- test/unit/Repositories/Planet.spec.ts
+++ test/unit/Repositories/Planet.spec.ts
@@ -6,14 +6,6 @@ import setupDatabase from '../../config/SetupDatabase';
 import { PlanetRepository } from '@Repositories';
 import { Planet } from '@Entities';
 
-beforeEach(async () => {
-	await setupDatabase();
-});
-
-afterEach(async () => {
-	await getConnection().close();
-});
-
 describe('Planet Repository Constructor', () => {
 	it('should be defined', () => {
 		expect(PlanetRepository).toBeDefined();
@@ -21,21 +13,26 @@ describe('Planet Repository Constructor', () => {
 });
 
 describe('Planet Repository Methods', () => {
-	it('should defend', async () => {
+	beforeEach(async () => {
+		await setupDatabase();
+	});
+
+	afterEach(async () => {
+		await getConnection().close();
+	});
+
+	it('Defend', async () => {
 		const entityManager = getManager();
 
 		const planet = await entityManager.findOne(Planet, { name: 'Alderaan' });
 
-		const damage = 1000;
+		const planetName = 'Alderaan'
+		const weaponName = 'Death Star'
 
-		const expected: number = planet.shield - damage;
+		const { damage, remainingShield } = await PlanetRepository.Defend(weaponName, planetName);
 
-		const params = {
-			damage,
-		};
+		const expectedShield = planet.shield - damage
 
-		const { alderaan } = await PlanetRepository.Defend(params.damage);
-
-		expect(alderaan.shield).toEqual(expected);
+		expect(remainingShield).toEqual(expectedShield);
 	});
 });
