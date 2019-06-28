439254ba4449807f63860313f74ef1640bd0668e
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-06-28 10:54:17 +0300

    Defend service spec

---
+++ test/unit/MicroServices/planet.spec.ts
@@ -0,0 +1,54 @@
+// Global Imports
+import { ServiceBroker } from 'moleculer';
+import { getManager, getConnection } from 'typeorm';
+
+// Local Imports
+import setupDatabase from '../../config/SetupDatabase';
+import { PlanetHelper } from '@Helper';
+import { Planet } from '@Entities/Planet';
+
+const PlanetService = require('../../../services/planet.service');
+
+const broker = new ServiceBroker({ logger: false });
+broker.createService(PlanetService);
+
+beforeAll(async () => {
+	await broker.start();
+})
+
+beforeEach(async () => {
+	await setupDatabase();
+});
+
+afterEach(async () => {
+	await getConnection().close();
+});
+
+afterAll(async () => {
+	await broker.stop();
+})
+
+describe('Test Defend service', () => {
+	describe('Defend method', async () => {
+		const params = {
+			planetName: 'Alderaan',
+			weaponName: 'Death Star'
+		}
+
+		it('when shield is up', async () => {
+			const { planetMessage } = await PlanetHelper.Defend(broker as any, params)
+
+			expect(planetMessage).toContain('Planet took')
+		});
+
+		it('when shield is down', async () => {
+			getManager().update(Planet, { name: 'Alderaan' }, { shield: 1 })
+
+			// First fire to break shield entirely
+			await PlanetHelper.Defend(broker as any, params);
+			const { planetMessage } = await PlanetHelper.Defend(broker as any, params);
+
+			expect(planetMessage).toEqual('Planet shield ruined! war is lost!')
+		})
+	});
+});
