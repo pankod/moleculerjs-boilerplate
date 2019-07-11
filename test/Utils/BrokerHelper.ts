5cf70c8976417ed5aee3b36496c8f7a79a2ec274
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-07-11 17:26:34 +0300

    Skip eslint for BrokerHelper imports

--- test/Utils/BrokerHelper.ts
+++ test/Utils/BrokerHelper.ts
@@ -3,8 +3,10 @@ import { ServiceBroker } from 'moleculer';
 //#endregion Global Imports
 
 //#region Local Imports
+/* eslint-disable */
 const AttackService = require('../../services/attack.service');
 const PlanetService = require('../../services/planet.service');
+/* eslint-enable */
 //#endregion Local Imports
 
 export namespace BrokerHelper {
