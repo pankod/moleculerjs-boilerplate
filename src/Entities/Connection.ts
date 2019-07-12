e63d4142f0a55a62ee486188b181821b8727ea6f
Omer Faruk APLAK <omeraplak@gmail.com>
2019-07-12 10:20:02 +0300

    add try catch for connection duplication fix

--- src/Entities/Connection.ts
+++ src/Entities/Connection.ts
@@ -2,11 +2,16 @@
 import { createConnection, Connection } from 'typeorm';
 //#endregion Global Imports
 
-export default async (): Promise<Connection> =>
-	await createConnection({
-		type: 'sqlite',
-		name: 'default',
-		database: './db.sqlite',
-		entities: [__dirname + '/*'],
-		synchronize: true,
-	});
+export default async (): Promise<Connection> => {
+	try {
+		return await createConnection({
+			type: 'sqlite',
+			name: 'default',
+			database: './db.sqlite',
+			entities: [__dirname + '/*'],
+			synchronize: true,
+		});
+	} catch (error) {
+		
+	}
+}
