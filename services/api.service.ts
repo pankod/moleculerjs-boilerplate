7b5e98b773763711b218bf76cbd4d7e088b37506
Austin Rivas <austinrivas@gmail.com>
2020-06-28 20:14:57 +0700

    better swagger docs

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,8 +1,166 @@
 //#region Global Imports
 import { ServiceSchema } from 'moleculer';
-import ApiGateway = require('moleculer-web');
+import * as ApiGateway from 'moleculer-web';
 //#endregion Global Imports
 
+/**
+ * @swagger
+ * 
+ * definitions:
+ * 
+ *   ErrorEntityNotFound:
+ *     type: object
+ *     required:
+ *       - name
+ *       - message
+ *       - code
+ *       - type
+ *     properties:
+ *       name:
+ *         type: string
+ *         example: "MoleculerError"
+ *       message:
+ *         type: string
+ *         example: "Resource can't be found with options: {\"where\":{\"name\":\"Pew Pew\"}}"
+ *       code:
+ *         type: integer
+ *         example: 404
+ *       type:
+ *         type: string
+ *         example: "Not Found"
+ * 
+ *   ErrorUnprocessableEntity:
+ *     type: object
+ *     required:
+ *       - name
+ *       - message
+ *       - code
+ *       - type
+ *       - data
+ *     properties:
+ *       name:
+ *         type: string
+ *         example: "required"
+ *       message:
+ *         type: string
+ *         example: "Parameters validation error!"
+ *       code:
+ *         type: integer
+ *         example: 422
+ *       type:
+ *         type: string
+ *         example: "VALIDATION_ERROR"
+ *       data:
+ *         type: array
+ *         items:
+ *           $ref: '#/definitions/ErrorUnprocessableEntityData'
+ * 
+ *   ErrorUnprocessableEntityData:
+ *     type: object
+ *     required:
+ *       - type
+ *       - message
+ *       - field
+ *       - nodeID
+ *       - action
+ *     properties:
+ *       type:
+ *         type: string
+ *         example: "required"
+ *       message:
+ *         type: string
+ *         example: "The 'planetName' field is required."
+ *       field:
+ *         type: string
+ *         example: "planetName"
+ *       nodeID:
+ *         type: string
+ *         example: "asus-ubuntu-3505"
+ *       action:
+ *         type: string
+ *         example: "attack.Fire"
+ * 
+ *   ErrorUncaught:
+ *     type: object
+ *     required:
+ *       - name
+ *       - message
+ *       - code
+ *     properties:
+ *       name:
+ *         type: string
+ *         example: "MoleculerError"
+ *       message:
+ *         type: string
+ *         example: ""
+ *       code:
+ *         type: integer
+ *         example: 500
+ * 
+ * parameters:
+ * 
+ *   weaponName:
+ *     type: string
+ *     name: weaponName
+ *     example: 'Death Star'
+ *     description: The weapon being used to attack.
+ * 
+ *   planetName:
+ *     type: string
+ *     name: planetName
+ *     example: 'Alderaan'
+ *     description: The planet being attacked.
+ * 
+ *   AttackPlanetParams:
+ *     in: body
+ *     name: params
+ *     required:
+ *       - weaponName
+ *       - planetName
+ *     schema:
+ *       type: object
+ *       properties:
+ *         weaponName:
+ *           $ref: '#/parameters/weaponName'
+ *         planetName:
+ *           $ref: '#/parameters/planetName'
+ *     description: The weapon being used and planet being attacked.
+ * 
+ * responses:
+ * 
+ *   UnprocessableEntity:
+ *     description: The request could not be processed.
+ *     headers:
+ *       content-type:
+ *         description: The Content-Type entity header is used to indicate the media type of the resource.
+ *         schema:
+ *           type: string
+ *           example: 'application/json; charset=utf-8'
+ *     schema:
+ *       $ref: '#/definitions/ErrorUnprocessableEntity'
+ * 
+ *   EntityNotFound:
+ *     description: The requested entity could not be found.
+ *     headers:
+ *       content-type:
+ *         description: The Content-Type entity header is used to indicate the media type of the resource.
+ *         schema:
+ *           type: string
+ *           example: 'application/json; charset=utf-8'
+ *     schema:
+ *       $ref: '#/definitions/ErrorEntityNotFound'
+ * 
+ *   UncaughtError:
+ *     description: The server encountered an unhandled error.
+ *     headers:
+ *       content-type:
+ *         description: The Content-Type entity header is used to indicate the media type of the resource.
+ *         schema:
+ *           type: string
+ *           example: 'application/json; charset=utf-8'
+ *     schema:
+ *       $ref: '#/definitions/ErrorUncaught'
+ */
 const ApiService: ServiceSchema = {
 	name: 'api',
 
--- services/attack.service.ts
+++ services/attack.service.ts
@@ -12,12 +12,77 @@ import connectionInstance from '@Entities/Connection';
 
 //#region Interface Imports
 import { IAttack } from '@Interfaces';
+import { exit } from 'process';
 //#endregion Interface Imports
 
+/**
+ * @swagger
+ * tags:
+ *   name: Attack Service
+ *   description: Attacks to the planet with given weapon.
+ */
 @Service({
 	name: 'attack',
 })
 class AttackService extends MoleculerService {
+  /**
+   * @swagger
+   * definitions:
+   * 
+   *   AttackResponseBody:
+   *     in: body
+   *     required:
+   *       - planetMessage
+   *       - weaponMessage
+   *     properties:
+   *       planetMessage:
+   *         type: string
+   *         example: 'Planet took 233 damage and has 87552 shield left.'
+   *       weaponMessage:
+   *         type: string
+   *         example: 'Death Star did 89 damage and left 986 ammo.'
+   * 
+   * responses:
+   * 
+   *   AttackResponse:
+   *     description: Ok
+   *     headers:
+   *       content-type:
+   *         description: The Content-Type entity header is used to indicate the media type of the resource.
+   *         schema:
+   *           type: string
+   *           example: 'application/json; charset=utf-8'
+   *       content-length:
+   *         description: The Content-Length entity-header field indicates the size of the entity-body.
+   *         schema:
+   *           type: integer
+   *           example: 132
+   *     schema:
+   *       type: object
+   *       $ref: '#/definitions/AttackResponseBody'
+   * 
+	 * paths:
+   * 
+	 *  /attack/Fire:
+	 *    post:
+	 *      description: Attacks to the planet with given weapon.
+   *      tags: [Attack Service]
+	 *      produces:
+	 *        - application/json
+	 *      consumes:
+	 *        - application/json
+	 *      parameters:
+   *        - $ref: '#/parameters/AttackPlanetParams'
+	 *      responses:
+	 *        200:
+	 *          $ref: '#/responses/AttackResponse'
+	 *        404:
+   *          $ref: '#/responses/EntityNotFound'
+	 *        422:
+   *          $ref: '#/responses/UnprocessableEntity'
+	 *        5XX:
+   *          $ref: '#/responses/UncaughtError'
+	 */
 	@Action({
 		params: {
 			weaponName: { type: 'string', min: 2 },
@@ -28,42 +93,13 @@ class AttackService extends MoleculerService {
 		const response = await this.FireMethod(ctx);
 
 		return response;
-	}
+  }
 
 	@Method
-	/**
-	 * @swagger
-	 *
-	 *  /attack/Fire:
-	 *    post:
-	 *      description: Attacks to the planet with given weapon.
-	 *      produces:
-	 *        - application/json
-	 *      consumes:
-	 *        - application/json
-	 *      parameters:
-	 *        - in: body
-	 *          name: params
-	 *          schema:
-	 *            type: object
-	 *            required:
-	 *              - weaponName
-	 *              - planetName
-	 *            properties:
-	 *              weaponName:
-	 *                type: string
-	 *                example: Death Star
-	 *              planetName:
-	 *                type: string
-	 *                example: Alderaan
-	 *      responses:
-	 *        200:
-	 *          description: Example attack result
-	 *        422:
-	 *          description: Missing parameters
-	 */
 	public async FireMethod(ctx: Context<IAttack.FireInDto>): Promise<IAttack.FireOutDto> {
-		const { planetName, weaponName } = ctx.params;
+    const { planetName, weaponName } = ctx.params;
+    
+    throw new Error();
 
 		const weapon = await WeaponRepository.Get(weaponName);
 
